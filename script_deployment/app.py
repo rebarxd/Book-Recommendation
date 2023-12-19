be303944be20d5f57af046757f11ffae7fe3e67b
My name <me@example.com>
2023-12-20 06:43:05 +0700

    Update final

--- script_deployment/app.py
+++ script_deployment/app.py
@@ -1,52 +1,21 @@
-from flask import Flask, request, jsonify
-import pickle
-import numpy as np
-
-popular_df = pickle.load(open('model/popular.pkl', 'rb'))
-pt = pickle.load(open('model/pt.pkl', 'rb'))
-books = pickle.load(open('model/books.pkl', 'rb'))
-similarity_scores = pickle.load(open('model/similarity_scores.pkl', 'rb'))
+from flask import Flask, request
+from library import recommend_library
+from book import get_popular_books,recommend_books
 
 app = Flask(__name__)
 
-def convert_to_python_types(data):
-    if isinstance(data, np.int64):
-        return int(data)
-    elif isinstance(data, np.generic):
-        return data.item()
-    elif isinstance(data, list):
-        return [convert_to_python_types(item) for item in data]
-    elif isinstance(data, dict):
-        return {key: convert_to_python_types(value) for key, value in data.items()}
-    else:
-        return data
-
 @app.route('/')
 def index():
-    return jsonify({
-        "book_name": convert_to_python_types(list(popular_df['Book-Title'].values)),
-        "author": convert_to_python_types(list(popular_df['Book-Author'].values)),
-        "votes": convert_to_python_types(list(popular_df['num_ratings'].values)),
-        "rating": convert_to_python_types(list(popular_df['avg_rating'].values))
-    })
+    return get_popular_books()
 
 @app.route('/recommend_books', methods=['GET'])
 def recommend():
     user_input = request.args.get('user_input')
-    index = np.where(pt.index == user_input)[0][0]
-    similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
+    return recommend_books(user_input)
 
-    data = []
-    for i in similar_items:
-        item = {}
-        temp_df = books[books['Book-Title'] == pt.index[i[0]]]
-        item["book_title"] = convert_to_python_types(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
-        item["author"] = convert_to_python_types(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
-
-        data.append(item)
-
-    return jsonify({"data": convert_to_python_types(data)})
-
+@app.route('/library_recommendation', methods=['GET'])
+def place_recommendation(latitude: float = -6.877568754589162, longitude: float = 107.61681917116401):
+    return recommend_library(latitude, longitude)
 
 if __name__ == '__main__':
     app.run()
--- script_deployment/book.py
+++ script_deployment/book.py
@@ -1,51 +1,50 @@
-from flask import Flask, render_template, request
 import pickle
 import numpy as np
-# from flask_ngrok import run_with_ngrok  # Import Ngrok module
+from flask import jsonify
 
+# Load data from pickle files
 popular_df = pickle.load(open('model/popular.pkl', 'rb'))
 pt = pickle.load(open('model/pt.pkl', 'rb'))
 books = pickle.load(open('model/books.pkl', 'rb'))
 similarity_scores = pickle.load(open('model/similarity_scores.pkl', 'rb'))
 
-app = Flask(__name__)
-# run_with_ngrok(app)  # Add Ngrok to Flask app
+def convert_to_python_types(data):
+    # Retrieves information about popular books.
+    if isinstance(data, np.int64):
+        return int(data)
+    elif isinstance(data, np.generic):
+        return data.item()
+    elif isinstance(data, list):
+        return [convert_to_python_types(item) for item in data]
+    elif isinstance(data, dict):
+        return {key: convert_to_python_types(value) for key, value in data.items()}
+    else:
+        return data
 
-@app.route('/')
-def index():
-    return render_template('./index.html',
-                           book_name=list(popular_df['Book-Title'].values),
-                           author=list(popular_df['Book-Author'].values),
-                           image=list(popular_df['Image-URL-M'].values),
-                           votes=list(popular_df['num_ratings'].values),
-                           rating=list(popular_df['avg_rating'].values)
-                           )
+#  Retrieves information about popular books.
+def get_popular_books():
+    return jsonify({
+        "book_name": convert_to_python_types(list(popular_df['Book-Title'].values)),
+        "author": convert_to_python_types(list(popular_df['Book-Author'].values)),
+        "votes": convert_to_python_types(list(popular_df['num_ratings'].values)),
+        "rating": convert_to_python_types(list(popular_df['avg_rating'].values))
+    })
 
-@app.route('/recommend')
-def recommend_ui():
-    return render_template('recommend.html')
+#  Recommends books based on user input.
+def recommend_books(user_input):
+    # # Find the index of the user input in the data
+    index = np.where(pt.index == user_input)[0][0]
+    # # Get similar items based on similarity scores
+    similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
 
+    data = []
+    for i in similar_items:
+        item = {}
+        #  # Extract information about recommended books
+        temp_df = books[books['Book-Title'] == pt.index[i[0]]]
+        item["book_title"] = convert_to_python_types(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
+        item["author"] = convert_to_python_types(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
 
-# @app.route('/recommend_books', methods=['POST'])
-# def recommend():
-#     user_input = request.form.get('user_input')
-#     index = np.where(pt.index == user_input)[0][0]
-#     similar_items = sorted(list(enumerate(similarity_scores[index])), key=lambda x: x[1], reverse=True)[1:5]
-
-#     data = []
-#     for i in similar_items:
-#         item = []
-#         temp_df = books[books['Book-Title'] == pt.index[i[0]]]
-#         item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Title'].values))
-#         item.extend(list(temp_df.drop_duplicates('Book-Title')['Book-Author'].values))
-#         item.extend(list(temp_df.drop_duplicates('Book-Title')['Image-URL-M'].values))
-
-#         data.append(item)
-
-#     print(data)
-
-#     return render_template('recommend.html', data=data)
-
-if __name__ == '__main__':
-    app.run()
-
+        data.append(item)
+    # JSON response containing recommended books.
+    return jsonify({"data": convert_to_python_types(data)})
